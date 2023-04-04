30fb7d7239a2bf8c7b1135f9a61426082a04156e
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-04-05 00:10:28 +0500

    Removed unwanted pellets by adding box Colliders and using Script

---
+++ Pacman/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    int NumberOfPellets = 28;
+    public float offset = 0.299f;
+    public float currentPellet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
